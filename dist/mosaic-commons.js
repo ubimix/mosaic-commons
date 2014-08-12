86d36b39fd80cd1b070aab978edbb56adde2f6d1
kotelnikov <mikhail.kotelnikov@ubimix.com>
2014-08-12 19:19:39 +0200

    Class inheritence was slightly changed

--- dist/mosaic-commons.js
+++ dist/mosaic-commons.js
@@ -107,7 +107,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	         * of the specified type.
 	         */
 	        isSubtype : function(type, includeThis) {
-	            if (!type || !type[TYPE_ID_KEY]) return false;
+	            if (!type || !type[TYPE_ID_KEY])
+	                return false;
 	            var result = false;
 	            for (var t = includeThis ? this : this[PARENT_KEY]; // 
 	            !result && !!t && t[TYPE_ID_KEY] !== undefined; t = t[PARENT_KEY]) {
@@ -121,13 +122,15 @@ return /******/ (function(modules) { // webpackBootstrap
 	         * object.
 	         */
 	        isSameType : function(type) {
-	            if (!type || !type[TYPE_ID_KEY]) return false;
+	            if (!type || !type[TYPE_ID_KEY])
+	                return false;
 	            return this[TYPE_ID_KEY] == type[TYPE_ID_KEY];
 	        },
 
 	        /** Returns true if the specified object is an instance of this class */
 	        hasInstance : function(obj) {
-	            if (!obj || !obj.instanceOf) return false;
+	            if (!obj || !obj.instanceOf)
+	                return false;
 	            return obj.instanceOf.call(obj, this);
 	        },
 
@@ -162,7 +165,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	        var Parent = Type[PARENT_KEY];
 	        _.extend(Type.prototype, this.instanceFields);
 	        if (Parent) {
-	            _.extend(Type.prototype, Parent.prototype);
+	            _.each(_.keys(Parent.prototype), function(key) {
+	                Type.prototype[key] = Parent.prototype[key];
+	            });
 	        }
 	        _.each(args, function(fields) {
 	            _.extend(Type.prototype, fields);
@@ -173,8 +178,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	    newType : function(Parent, args) {
 	        function Constructor() {
 	            if (this.initialize) {
-	            	this.initialize.apply(this, arguments);
-	        	}
+	                this.initialize.apply(this, arguments);
+	            }
 	        }
 	        return Constructor;
 	    },
--- src/Class.js
+++ src/Class.js
@@ -37,7 +37,8 @@ _.extend(MetaClass.prototype, {
          * of the specified type.
          */
         isSubtype : function(type, includeThis) {
-            if (!type || !type[TYPE_ID_KEY]) return false;
+            if (!type || !type[TYPE_ID_KEY])
+                return false;
             var result = false;
             for (var t = includeThis ? this : this[PARENT_KEY]; // 
             !result && !!t && t[TYPE_ID_KEY] !== undefined; t = t[PARENT_KEY]) {
@@ -51,13 +52,15 @@ _.extend(MetaClass.prototype, {
          * object.
          */
         isSameType : function(type) {
-            if (!type || !type[TYPE_ID_KEY]) return false;
+            if (!type || !type[TYPE_ID_KEY])
+                return false;
             return this[TYPE_ID_KEY] == type[TYPE_ID_KEY];
         },
 
         /** Returns true if the specified object is an instance of this class */
         hasInstance : function(obj) {
-            if (!obj || !obj.instanceOf) return false;
+            if (!obj || !obj.instanceOf)
+                return false;
             return obj.instanceOf.call(obj, this);
         },
 
@@ -92,7 +95,9 @@ _.extend(MetaClass.prototype, {
         var Parent = Type[PARENT_KEY];
         _.extend(Type.prototype, this.instanceFields);
         if (Parent) {
-            _.extend(Type.prototype, Parent.prototype);
+            _.each(_.keys(Parent.prototype), function(key) {
+                Type.prototype[key] = Parent.prototype[key];
+            });
         }
         _.each(args, function(fields) {
             _.extend(Type.prototype, fields);
@@ -103,8 +108,8 @@ _.extend(MetaClass.prototype, {
     newType : function(Parent, args) {
         function Constructor() {
             if (this.initialize) {
-            	this.initialize.apply(this, arguments);
-        	}
+                this.initialize.apply(this, arguments);
+            }
         }
         return Constructor;
     },
